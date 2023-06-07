8b49bc058f9c1fd863da5173e79a96c9436961a4
gabb222 <gabriella.bruno@theiascientific.com>
2023-06-07 12:34:24 -0400

    Changed function to take a single file

--- tspng/extraction.py
+++ tspng/extraction.py
@@ -1,15 +1,12 @@
+#import statements
+from PIL import Image
+
+import json
+
 def extraction(fname):
-
-    #import statements
-    from PIL import Image
-
-    import json
-
     #open
     im=Image.open(fname)
     meta=im.text
     #load
     dict=json.loads(meta['application/vnd.theiascope.io+json'])
     return dict
-
-extraction('20230510T192247Z.722_crimson-notebook (PML).ts.png')

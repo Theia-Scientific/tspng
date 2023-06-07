077c983f0ded3444fbeaf546185e4814e3b7ad70
gabb222 <gabriella.bruno@theiascientific.com>
2023-06-07 12:31:45 -0400

    Moved import statements up

--- tspng/extraction.py
+++ tspng/extraction.py
@@ -1,22 +1,15 @@
-def extraction():
+def extraction(fname):
 
     #import statements
     from PIL import Image
 
     import json
-    import os
 
-    #path to data
-    filepath = os.path.dirname(os.path.abspath(__file__))+'/TSDATA'
-    #list of files of interest
-    filenames = os.listdir(filepath)
-    data = []
-    for filename in filenames:
-        #open
-        im = Image.open(filepath+filename)
-        meta=im.text
-        #load
-        dict=json.loads(meta['application/vnd.theiascope.io+json'])
-        #append
-        data.append(dict)
-    return data
+    #open
+    im=Image.open(fname)
+    meta=im.text
+    #load
+    dict=json.loads(meta['application/vnd.theiascope.io+json'])
+    return dict
+
+extraction('20230510T192247Z.722_crimson-notebook (PML).ts.png')

6e31bf6b7407fb612ddcbc81478bfb2fb2d3716d
gabb222 <gabriella.bruno@theiascientific.com>
2023-06-20 16:17:05 -0400

    Change name of test to `test_extract`

--- tspng/test_extraction.py
+++ tspng/test_extraction.py
@@ -5,6 +5,6 @@ from tspng.extraction import extract
 def test_hello_world():
     print('Hello World!')
 
-def test_extraction():
+def test_extract():
     test_data = extract('tests/assets/example_file.ts.png')
     assert list(test_data.keys())==['info','licenses','images','annotations','models','categories']

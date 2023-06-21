dce8143890045655f33b6016d686034ab10d8f17
Gabriella Bruno <60561619+gabb222@users.noreply.github.com>
2023-06-21 10:27:50 -0400

    Add real unit test (#11)
    * Added first real unit test
    
    * Adjusted extraction function to take path
    
    * Fix module import

---
+++ tspng/test_extraction.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+from tspng.extraction import extract
+
+def test_hello_world():
+    print('Hello World!')
+
+def test_extract():
+    test_data = extract('tests/assets/example_file.ts.png')
+    assert list(test_data.keys())==['info','licenses','images','annotations','models','categories']
--- tspng/test_tspng.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-from . import add
-
-def test_hello_world():
-    print('Hello World!')
-
-def test_add():
-    assert add(1,1)==2

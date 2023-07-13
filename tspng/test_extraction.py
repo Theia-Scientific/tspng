beba1e30066f06c476d5f6222634a99958111aa5
gabb222 <gabriella.bruno@theiascientific.com>
2023-07-13 13:38:25 -0400

    Added test for `extract_from_files` function

--- tspng/test_extraction.py
+++ tspng/test_extraction.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python3
 
-from tspng.extraction import extract_from_file
+from tspng.extraction import extract_from_file,extract_from_files
 
 def test_hello_world():
     print('Hello World!')
 
 def test_extract_from_file():
-    test_data = extract_from_file('tests/assets/example_file.ts.png')
+    test_data = extract_from_file('tests/assets/example_file_1.ts.png')
     assert list(test_data.keys())==['info','licenses','images','annotations','models','categories']
+
+def test_extract_from_files():
+    test_data = extract_from_files(['tests/assets/example_file_1.ts.png','tests/assets/example_file_2.ts.png'])
+    assert list(test_data.keys())==['tests/assets/example_file_1.ts.png','tests/assets/example_file_2.ts.png']
+    

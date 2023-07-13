67ffd983590895ec36e2f9d72e08b01816b9fc5b
gabb222 <gabriella.bruno@theiascientific.com>
2023-07-13 13:38:01 -0400

    Added `extract_from_files` function

--- tspng/extraction.py
+++ tspng/extraction.py
@@ -13,3 +13,16 @@ def extract_from_file(path, mime_type=MIME_TYPE):
     #load
     dict=json.loads(meta[mime_type])
     return dict
+
+def extract_from_files(paths, mime_type=MIME_TYPE):
+    nested_dict = {}
+    for path in paths:
+        #open
+        abs_path=os.path.abspath(path)
+        im=Image.open(abs_path)
+        meta=im.text
+        #load
+        dict=json.loads(meta[mime_type])
+        #add to nested dictionary
+        nested_dict[path]=dict
+    return nested_dict

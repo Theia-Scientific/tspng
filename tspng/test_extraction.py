efc42bd159fa17950ab94fdd84f96701510edd4e
gabb222 <gabriella.bruno@theiascientific.com>
2023-07-13 14:43:24 -0400

    Added docstring documentation to functions

--- tspng/test_extraction.py
+++ tspng/test_extraction.py
@@ -6,10 +6,16 @@ def test_hello_world():
     print('Hello World!')
 
 def test_extract_from_file():
+    '''
+    Tests the dictionary keys from an example TSPNG file path.
+    '''
     test_data = extract_from_file('tests/assets/example_file_1.ts.png')
     assert list(test_data.keys())==['info','licenses','images','annotations','models','categories']
 
 def test_extract_from_files():
+    '''
+    Tests the dictionary keys from a list of example TSPNG file paths.
+    '''
     test_data = extract_from_files(['tests/assets/example_file_1.ts.png','tests/assets/example_file_2.ts.png'])
     assert list(test_data.keys())==['tests/assets/example_file_1.ts.png','tests/assets/example_file_2.ts.png']
     

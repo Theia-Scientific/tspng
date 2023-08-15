c70d52a85cf87214f6aebc8bfb7bd0388ed27964
gabb222 <gabriella.bruno@theiascientific.com>
2023-08-15 13:24:57 -0400

    Add tests for parent `extract` function

--- tspng/test_extraction.py
+++ tspng/test_extraction.py
@@ -1,7 +1,23 @@
 #!/usr/bin/env python3
 
 from io import BytesIO
-from tspng.extraction import extract_from_bytes,extract_from_file,extract_from_files
+from tspng.extraction import extract,extract_from_bytes,extract_from_file,extract_from_files
+
+def test_extract():
+    '''
+    Tests the dictionary keys from an example TSPNG file path.
+    '''
+    #test bytes
+    with open('tests/assets/example_file_1.ts.png', 'rb') as fh:
+        buf = BytesIO(fh.read())
+        test_data = extract_from_bytes(buf)
+        assert list(test_data.keys())==['info','licenses','images','annotations','models','categories']
+    #test file
+    test_data = extract('tests/assets/example_file_1.ts.png')
+    assert list(test_data.keys())==['info','licenses','images','annotations','models','categories']
+    #test list of files
+    test_data = extract_from_files(['tests/assets/example_file_1.ts.png','tests/assets/example_file_2.ts.png'])
+    assert list(test_data.keys())==['tests/assets/example_file_1.ts.png','tests/assets/example_file_2.ts.png']
 
 def test_extract_from_bytes():
     '''

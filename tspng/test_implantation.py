467db7bd4d32e303282d6f639c5cc475ac689d9f
Gabriella Bruno <gbruno527@gmail.com>
2023-10-31 15:32:01 -0400

    Add unit tests for implantation modules

--- tspng/test_implantation.py
+++ tspng/test_implantation.py
@@ -5,7 +5,7 @@ import pathlib
 import pytest
 
 from io import BytesIO
-from tspng.implantation import implant,implant_to_file
+from tspng.implantation import implant,implant_from_file
 from tspng.extraction import extract
 
 def test_implant():
@@ -13,28 +13,27 @@ def test_implant():
     Tests the dictionary keys from an example TSPNG file path.
     '''
     #test file
-    implant('tests/assets/coco_data.txt','tests/assets/empty.png')
-    #test_data = extract('tests/assets/coco_data.txt+tests/assets/empty.png')
-    test_data = extract('tests/assets/empty.png')
+    implant('tests/assets/coco_data.json','tests/assets/empty.png')
+    test_data = extract('tests/assets/empty.png_MOD')
     assert list(test_data.keys())==['info','licenses','images','annotations','models','categories']
 
 def test_implant_fails():
     with pytest.raises(TypeError):
         implant('Test for failure')
 
-def test_implant_to_file():
+def test_implant_from_file():
     '''
     Tests the dictionary keys from an example TSPNG file path.
     '''
-    implant_to_file('tests/assets/coco_data.txt','tests/assets/empty.png')
+    implant_from_file('tests/assets/coco_data.json','tests/assets/empty.png')
     #test_data = extract('tests/assets/coco_data.txt+tests/assets/empty.png')
-    test_data = extract('tests/assets/empty.png')
+    test_data = extract('tests/assets/empty.png_MOD')
     assert list(test_data.keys())==['info','licenses','images','annotations','models','categories']
 
-def test_implant_to_file_fails():
+def test_implant_from_file_fails():
     #test folder instead of file
     with pytest.raises(Exception):
-        implant_to_file('Random/path.png')
+        implant_from_file('Random/path.png')
     #test file does not exist
     with pytest.raises(Exception):
-        implant_to_file('tests/assets')
+        implant_from_file('tests/assets')

180c2270a879bb108471400652762d17018b0819
Christopher Field <chris.field@theiascientific.com>
2024-03-29 10:50:10 -0400

    Fix tests

--- tspng/test_implantation.py
+++ tspng/test_implantation.py
@@ -4,7 +4,7 @@ import os
 import pytest
 
 from pathlib import Path
-from tspng.implantation import implant, implant_from_file
+from tspng.implantation import implant, implant_into_file
 from tspng.extraction import extract
 
 
@@ -31,12 +31,12 @@ def test_implant_fails():
         implant("Test for failure", "tests/assets/empty.png")
 
 
-def test_implant_from_file():
+def test_implant_into_file():
     """
     Tests the dictionary keys from an example TSPNG file path.
     """
 
-    implant_from_file("tests/assets/coco_data.json", "tests/assets/empty.png")
+    implant_into_file("tests/assets/coco_data.json", "tests/assets/empty.png")
     test_data = extract(Path("tests/assets/empty.ts.png"))
     assert list(test_data.keys()) == [
         "info",
@@ -51,7 +51,7 @@ def test_implant_from_file():
 
 def test_implant_from_file_fails_with_folder():
     with pytest.raises(Exception):
-        implant_from_file("tests/assets", "tests/assets/empty.png")
+        implant_into_file("tests/assets", "tests/assets/empty.png")
 
 
 def test_implant_from_file_fails_existence():

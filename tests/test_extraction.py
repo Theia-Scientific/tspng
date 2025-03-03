4f4f11b47de359643021b80c6a37ed5967844168
Christopher Field <chris.field@theiascientific.com>
2025-03-03 15:26:03 -0500

    Change to fixtures

--- tests/test_extraction.py
+++ tests/test_extraction.py
@@ -28,6 +28,9 @@ def empty_jpeg_path(tmp_path) -> Path:
     image.save(empty_jpeg_path, format="JPEG")
     return empty_jpeg_path
 
+@pytest.fixture
+def example_file_1_url() -> str:
+    return "https://bounding-box-instructions.s3.amazonaws.com/example_file_1.ts.png"
 
 def test_extract_with_file_path(example_file_1_path):
     test_data = extract(example_file_1_path)
@@ -68,10 +71,8 @@ def test_extract_with_folder(assets_directory_path, example_file_1_path, example
     assert str(example_file_2_path) in sorted(list(test_data.keys()))
 
 
-def test_extract_with_url():
-    test_data = extract(
-        "https://bounding-box-instructions.s3.amazonaws.com/example_file_1.ts.png"
-    )
+def test_extract_with_url(example_file_1_url):
+    test_data = extract(example_file_1_url)
     assert list(test_data.keys()) == [
         "info",
         "licenses",
@@ -101,11 +102,11 @@ def test_extract_fails():
         extract("Test for failure")
 
 
-def test_extract_from_bytes():
+def test_extract_from_bytes(example_file_1_path):
     """
     Tests the dictionary keys from an example TS byte stream.
     """
-    with open("tests/assets/example_file_1.ts.png", "rb") as fh:
+    with open(example_file_1_path, "rb") as fh:
         buf = BytesIO(fh.read())
         test_data = extract_from_bytes(buf)
         assert list(test_data.keys()) == [
@@ -118,11 +119,11 @@ def test_extract_from_bytes():
         ]
 
 
-def test_extract_from_file():
+def test_extract_from_file(example_file_1_path):
     """
     Tests the dictionary keys from an example TSPNG file path.
     """
-    test_data = extract_from_file(Path("tests/assets/example_file_1.ts.png"))
+    test_data = extract_from_file(example_file_1_path)
     assert list(test_data.keys()) == [
         "info",
         "licenses",
@@ -138,34 +139,30 @@ def test_extract_from_file_not_exists_fails():
         extract_from_file(Path("Random/path.png"))
 
 
-def test_extract_from_file_with_directory_fails():
+def test_extract_from_file_with_directory_fails(assets_directory_path):
     with pytest.raises(PathIsNotAFile):
-        extract_from_file(Path("tests/assets"))
+        extract_from_file(assets_directory_path)
 
 
-def test_extract_from_files():
+def test_extract_from_files(example_file_1_path, example_file_2_path):
     """
     Tests the dictionary keys from a list of example TSPNG file paths.
     """
-    test_data = extract_from_files(
-        [
-            Path("tests/assets/example_file_1.ts.png"),
-            Path("tests/assets/example_file_2.ts.png"),
+    files = [
+            example_file_1_path,
+            example_file_2_path,
         ]
-    )
-    assert sorted(list(test_data.keys())) == [
-        Path("tests/assets/example_file_1.ts.png"),
-        Path("tests/assets/example_file_2.ts.png"),
-    ]
+    test_data = extract_from_files(files)
+    assert sorted(list(test_data.keys())) == files
 
 
-def test_extract_from_folder():
+def test_extract_from_folder(assets_directory_path, example_file_1_path, example_file_2_path):
     """
     Tests the dictionary keys from a list of example TSPNG file paths.
     """
-    test_data = extract_from_folder(Path("tests/assets"))
-    assert "tests/assets/example_file_1.ts.png" in sorted(list(test_data.keys()))
-    assert "tests/assets/example_file_2.ts.png" in sorted(list(test_data.keys()))
+    test_data = extract_from_folder(assets_directory_path)
+    assert str(example_file_1_path) in sorted(list(test_data.keys()))
+    assert str(example_file_2_path) in sorted(list(test_data.keys()))
 
 
 def test_extract_from_folder_fails(example_file_1_path):
@@ -178,13 +175,11 @@ def test_extract_from_folder_fails_with_empty_files(tmp_path):
         extract_from_folder(tmp_path)
 
 
-def test_extract_from_url():
+def test_extract_from_url(example_file_1_url):
     """
     Tests the dictionary keys from an example TSPNG url.
     """
-    test_data = extract_from_url(
-        "https://bounding-box-instructions.s3.amazonaws.com/example_file_1.ts.png"
-    )
+    test_data = extract_from_url(example_file_1_url)
     assert list(test_data.keys()) == [
         "info",
         "licenses",

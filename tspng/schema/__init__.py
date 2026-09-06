4c0d37c8d96b34e786a8e612f7d8ee11555a1839
Christopher Field <chris.field@theiascientific.com>
2026-09-06 13:37:00 -0400

    Add Text schema

--- tspng/schema/__init__.py
+++ tspng/schema/__init__.py
@@ -2,42 +2,31 @@
 
 from __future__ import annotations
 
-import json
+from pydantic import BaseModel, Field, TypeAdapter
+from tspng.schema import coco, generic, text, ts
+from typing import Annotated, TypeAlias
 
-from pydantic import BaseModel, TypeAdapter
-from tspng.schema import coco, generic, ts
-
-KnownJson = coco.Json | ts.Json
-JsonData = KnownJson | generic.Json
+Data: TypeAlias = ts.Json | coco.Json | generic.Json | str
 
 
 class Metadata(BaseModel):
-    data: JsonData | str
+    data: Data
     mime_type: str
 
     @staticmethod
-    def from_json(json_data: generic.Json, mime_type: str | None) -> Metadata:
-        type_adapter = TypeAdapter(JsonData)
+    def from_data(data: Data, mime_type: str | None) -> Metadata:
+        type_adapter = TypeAdapter(Annotated[Data, Field(union_mode="left_to_right")])
         if mime_type is None:
-            data = type_adapter.validate_python(json_data)
-            if isinstance(data, coco.Json):
-                return Metadata(data=data, mime_type=coco.MIME_TYPE)
-            elif isinstance(data, ts.Json):
-                return Metadata(data=data, mime_type=ts.MIME_TYPE)
+            value = type_adapter.validate_python(data)
+            if isinstance(value, coco.Json):
+                return Metadata(data=value, mime_type=coco.MIME_TYPE)
+            elif isinstance(value, ts.Json):
+                return Metadata(data=value, mime_type=ts.MIME_TYPE)
+            elif isinstance(value, dict):
+                return Metadata(data=value, mime_type=generic.MIME_TYPE)
             else:
-                return Metadata(data=data, mime_type=generic.MIME_TYPE)
+                return Metadata(data=value, mime_type=text.MIME_TYPE)
         else:
             return Metadata(
-                data=type_adapter.validate_python(json_data), mime_type=mime_type
+                data=type_adapter.validate_python(data), mime_type=mime_type
             )
-
-    @property
-    def text(self) -> str:
-        if isinstance(self.data, coco.Json):
-            return self.data.model_dump_json()
-        elif isinstance(self.data, ts.Json):
-            return self.data.model_dump_json()
-        elif isinstance(self.data, dict):
-            return json.dumps(self.data)
-        else:
-            return self.data

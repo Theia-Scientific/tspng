fcf8d82aa1e0dcbf86341101405784cc6e1be3ac
Christopher Field <chris.field@theiascientific.com>
2026-09-07 11:51:09 -0400

    Remove unnecessary custom errors

--- tspng/__init__.py
+++ tspng/__init__.py
@@ -1,20 +1,6 @@
-import os
-
 from importlib import metadata
 
 __app_name__ = "tspng"
 __version__ = metadata.version(__package__ or __name__)
 
 PNG_FILE_EXT: str = ".png"
-
-
-class PathDoesNotExist(Exception):
-    def __init__(self, path: os.PathLike[str]):
-        self.path: os.PathLike[str] = path
-        super().__init__()
-
-
-class PathIsNotAFile(Exception):
-    def __init__(self, path: os.PathLike[str]):
-        self.path: os.PathLike[str] = path
-        super().__init__()

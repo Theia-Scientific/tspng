4a35bd3f1eee9b49950ccae84f25b2fcb7abc46d
Christopher Field <chris.field@theiascientific.com>
2026-09-09 16:42:20 -0400

    Change v2 schema

--- tspng/schema/ts/v2.py
+++ tspng/schema/ts/v2.py
@@ -1,12 +1,23 @@
 #!/usr/bin/env python3
 
+from __future__ import annotations
+
 from pydantic import BaseModel
 from typing import Any, Literal
 
 
-class Attributes(BaseModel):
-    digital_micrograph: dict[str, Any] | None = None
-    imagej: dict[str, Any] | None = None
+class Annotation(BaseModel):
+    created: str
+    confidence: float
+    database_id: int
+    height: tuple[float, int]
+    label: Class
+    ignore: bool = False
+    index: int | None = None
+    segmentation: list[list[int | float]]
+    tracking_id: int | None = None
+    uuid: str
+    width: tuple[float, int]
 
 
 class Class(BaseModel):
@@ -16,37 +27,20 @@ class Class(BaseModel):
 
 class FieldOfView(BaseModel):
     created: str
-    height_n: float = 1.0
-    width_n: float = 1.0
-    x_n: float = 0.0
-    y_n: float = 0.0
+    height: tuple[float, int]
+    width: tuple[float, int]
+    x: tuple[float, int]
+    y: tuple[float, int]
 
 
 class Image(BaseModel):
-    attributes: Attributes | None = None
-    created: str
-    origin: str
-    original_height_px: int
-    original_media_filename: str | None = None
-    original_media_path: str | None = None
-    original_width_px: int
+    original: Original
     uuid: str
 
 
-class Annotation(BaseModel):
-    created: str
-    confidence: float
-    database_id: int
-    height_n: float
-    label: Class
-    ignore: bool = False
-    index: int | None = None
-    segmentation: list[list[int | float]]
-    tracking_id: int | None = None
-    uuid: str
-    width_n: float
-    x_n: float
-    y_n: float
+class Media(BaseModel):
+    name: str
+    path: str
 
 
 class Model(BaseModel):
@@ -60,17 +54,21 @@ class Model(BaseModel):
     variant: str
 
 
+class Original(BaseModel):
+    dimensions: tuple[int, int]
+    media: Media
+
+
 class Units(BaseModel):
     abbr: str
     name: str
 
 
 class ScaleBar(BaseModel):
-    length_n: float
-    length_e: float
+    length: tuple[float, float, int]
     units: Units
-    x_n: float | None = None
-    y_n: float | None = None
+    x: tuple[float, int]
+    y: tuple[float, int]
 
 
 class Json(BaseModel):

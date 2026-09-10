5786b7c73e324157f42d3d54ee1bfb4680fdb124
Christopher Field <chris.field@theiascientific.com>
2026-09-10 11:43:34 -0400

    Fix tests

--- tests/test_extraction.py
+++ tests/test_extraction.py
@@ -214,13 +214,31 @@ def test_extract_from_files(example_file_1_path: Path, example_file_2_path: Path
     result = extract_from_files(files)
     assert isinstance(result, dict)
     assert example_file_1_str in result
-    assert isinstance(result[example_file_1_str], Embedded)
-    assert result[example_file_1_str].mime_type == TS_MIME_TYPE
-    assert isinstance(result[example_file_1_str].data, v1.Json)
+    assert isinstance(result[example_file_1_str], Metadata)
+    assert result[example_file_1_str].author is not None
+    assert result[example_file_1_str].comment is not None
+    assert result[example_file_1_str].copyright is not None
+    assert result[example_file_1_str].creation_time is not None
+    assert result[example_file_1_str].description is not None
+    assert result[example_file_1_str].title is not None
+    assert result[example_file_1_str].software is not None
+    assert result[example_file_1_str].source is not None
+    assert len(result[example_file_1_str].embedded) == 1
+    assert result[example_file_1_str].embedded[0].mime_type == TS_MIME_TYPE
+    assert isinstance(result[example_file_1_str].embedded[0].data, v1.Json)
     assert example_file_2_str in result
-    assert isinstance(result[example_file_2_str], Embedded)
-    assert result[example_file_1_str].mime_type == TS_MIME_TYPE
-    assert isinstance(result[example_file_2_str].data, v1.Json)
+    assert isinstance(result[example_file_2_str], Metadata)
+    assert result[example_file_2_str].author is not None
+    assert result[example_file_2_str].comment is not None
+    assert result[example_file_2_str].copyright is not None
+    assert result[example_file_2_str].creation_time is not None
+    assert result[example_file_2_str].description is not None
+    assert result[example_file_2_str].title is not None
+    assert result[example_file_2_str].software is not None
+    assert result[example_file_2_str].source is not None
+    assert len(result[example_file_2_str].embedded) == 1
+    assert result[example_file_2_str].embedded[0].mime_type == TS_MIME_TYPE
+    assert isinstance(result[example_file_2_str].embedded[0].data, v1.Json)
 
 
 def test_extract_from_folder(
@@ -231,13 +249,31 @@ def test_extract_from_folder(
     result = extract_from_folder(assets_directory_path)
     assert isinstance(result, dict)
     assert example_file_1_str in result
-    assert isinstance(result[example_file_1_str], Embedded)
-    assert result[example_file_1_str].mime_type == TS_MIME_TYPE
-    assert isinstance(result[example_file_1_str].data, v1.Json)
+    assert isinstance(result[example_file_1_str], Metadata)
+    assert result[example_file_1_str].author is not None
+    assert result[example_file_1_str].comment is not None
+    assert result[example_file_1_str].copyright is not None
+    assert result[example_file_1_str].creation_time is not None
+    assert result[example_file_1_str].description is not None
+    assert result[example_file_1_str].title is not None
+    assert result[example_file_1_str].software is not None
+    assert result[example_file_1_str].source is not None
+    assert len(result[example_file_1_str].embedded) == 1
+    assert result[example_file_1_str].embedded[0].mime_type == TS_MIME_TYPE
+    assert isinstance(result[example_file_1_str].embedded[0].data, v1.Json)
     assert example_file_2_str in result
-    assert isinstance(result[example_file_2_str], Embedded)
-    assert result[example_file_1_str].mime_type == TS_MIME_TYPE
-    assert isinstance(result[example_file_2_str].data, v1.Json)
+    assert isinstance(result[example_file_2_str], Metadata)
+    assert result[example_file_2_str].author is not None
+    assert result[example_file_2_str].comment is not None
+    assert result[example_file_2_str].copyright is not None
+    assert result[example_file_2_str].creation_time is not None
+    assert result[example_file_2_str].description is not None
+    assert result[example_file_2_str].title is not None
+    assert result[example_file_2_str].software is not None
+    assert result[example_file_2_str].source is not None
+    assert len(result[example_file_2_str].embedded) == 1
+    assert result[example_file_2_str].embedded[0].mime_type == TS_MIME_TYPE
+    assert isinstance(result[example_file_2_str].embedded[0].data, v1.Json)
 
 
 def test_extract_from_folder_fails(example_file_1_path: Path):
@@ -252,9 +288,18 @@ def test_extract_from_folder_fails_with_empty_files(tmp_path: Path):
 
 def test_extract_from_url(example_file_1_url: str):
     result = extract_from_url(example_file_1_url)
-    assert isinstance(result, Embedded)
-    assert result.mime_type == TS_MIME_TYPE
-    assert isinstance(result.data, v1.Json)
+    assert isinstance(result, Metadata)
+    assert result.author is not None
+    assert result.comment is not None
+    assert result.copyright is not None
+    assert result.creation_time is not None
+    assert result.description is not None
+    assert result.title is not None
+    assert result.software is not None
+    assert result.source is not None
+    assert len(result.embedded) == 1
+    assert result.embedded[0].mime_type == TS_MIME_TYPE
+    assert isinstance(result.embedded[0].data, v1.Json)
 
 
 def test_extract_from_url_fails():
